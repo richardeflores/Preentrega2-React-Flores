9f3ce83fc672b7f980d2002560fd04c62d4c3831
Richard Flores <lcdo.richardflores@gmail.com>
2024-09-24 10:18:55 -0300

    cambio url vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "https://richardeflores.github.io/preentrega2-React-Flores/",
+	base: "https://richardeflores.github.io/Preentrega2-React-Flores/",
 });
